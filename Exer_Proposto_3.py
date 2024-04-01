056d2700c05eec67135cad391b3141fca72873d4
annac@NITRO_ACR <annaclararenno@gmail.com>
2024-04-01 14:22:00 -0300

    Ecercicio Proposto 3

--- Exer_Proposto_3.py
+++ Exer_Proposto_3.py
@@ -1,3 +1,45 @@
 import numpy as np
 
-arr = np.loadtxt('arquivos/space.csv', delimiter=';', dtype=str,encoding='uft-8')
+arr = np.loadtxt('space.csv', delimiter=';', dtype='str', encoding='utf-8', skiprows=1)
+
+#1
+failureCount = np.char.find(arr[:,-1], 'Success').sum() * (-1)
+
+totalCount = arr[:,-1].size
+
+porcentagem = ((1-(failureCount/totalCount))*100)
+print(round(porcentagem,2), "%")
+
+
+#2
+gastos = arr[:, -2].astype(float)
+
+gastoTotal = gastos[gastos > 0].sum()
+
+available = gastos[gastos > 0].size
+
+media = gastoTotal / available
+print(round(media,2))
+
+#3
+local= arr[:,2]
+
+USAs = np.char.count(local, 'USA').sum()
+
+print(USAs, ' missões')
+
+#4
+
+spacex = arr[arr[:,1] == 'SpaceX']
+
+maisCara = spacex[spacex[:,-2].astype(float).argmax()]
+
+print(maisCara)
+
+#5
+
+empresa, counts = np.unique(arr[:,1], return_counts=True)
+
+print('Quantidade de missões:')
+for i in range(empresa.size):
+    print(empresa[i], ':', counts[i])
